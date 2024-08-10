4eb85404f0fe9bd12bb4a931088f4cc29dd06a76
finote2021 <finotepawloss29@gmail.com>
2024-08-10 14:27:36 -0700

    backEnd deployement source added

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -10,6 +10,9 @@
 
 import axios from "axios";
 const axiosInstance = axios.create({
+  //local instance of firebase functions
   baseURL: "http://127.0.0.1:5001/clone-559ec/us-central1/api",
+  //deployed version of amazon clone on render.com
+  baseURL: "https://amazoncloneiducational-backend.onrender.com/",
 });
 export { axiosInstance };
